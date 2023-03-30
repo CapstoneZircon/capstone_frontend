3a39cfb5914a8807552e4132859af10fc766e313
suphasonnuk <suphasonnuk@gmail.com>
2023-03-30 15:28:32 +0700

    First Commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import React from 'react';
+import './App.css';
+import ButtonFeed from './components/Button/ButtonFeed'
+
+function App() {
+  return (
+    <div className="App">
+      <ButtonFeed></ButtonFeed>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/Button/ButtonFeed.tsx
@@ -0,0 +1,10 @@
+import React from 'react'
+import { Button } from '@material-tailwind/react'
+
+const ButtonFeed = () => {
+
+    return(
+        <Button variant='filled'> Hello world </Button>
+    )
+
+}; export default ButtonFeed;
