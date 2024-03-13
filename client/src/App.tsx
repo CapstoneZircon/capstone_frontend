307705f4e92a324e9c055188f8a16dd545bf74c7
Putthipong Tiamduangtawan <pud987@gmail.com>
2024-03-13 15:54:16 +0700

    save before organize

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,16 +1,12 @@
 import './App.css';
 import HomePage from './pages/home';
 import LoginPage from './pages/login';
-import SaleOrder from './pages/salesOrder';
 import Footage from './pages/footage_main';
-import Records1 from './pages/records_old_pagination';
 import Records from './pages/records_antd'
 import VideoFootagePage from './pages/footage_video';
-import VideoFootagePagec from './pages/footage_video';
 
 import React, { useState, useEffect, useContext } from 'react';
 import { Routes, BrowserRouter as Router, Route } from 'react-router-dom';
-import { AuthContext } from './context/AuthContext';
 import { Navigate } from 'react-router-dom';
 
 const App = () => {
